680dd8a90e33621b9d9e6549a5dfdeaf190b2248
ChenJialong <2833275565@qq.com>
2021-10-25 14:33:01 +0800

    Try to git-push

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,5 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+    }//onCreate()这是第一个回调，在活动第一次创建时调用
 }
