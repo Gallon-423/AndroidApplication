ab3953823fa8709a749197395591da741f26bf17
ChenJialong <2833275565@qq.com>
2021-12-17 21:50:01 +0800

    Layout learning~

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.myapplication
 
+import android.content.ComponentName
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }//onCreate()这是第一个回调，在活动第一次创建时调用
+    }//onCreate()这是第一个回调，在活动第一次创建时调用.
+
+
 }
